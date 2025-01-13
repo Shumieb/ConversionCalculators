86bda580adec4007281f6e96fc0c72cbe4e675e9
Shumieb <shumiebc@gmail.com>
2025-01-13 17:03:42 +0000

    fixed weight ui

--- utils.js
+++ utils.js
@@ -79,3 +79,70 @@ export const FahrenheitToKelvin = (fahrenheit) => {
     return (fahrenheit - 32) * (5 / 9) + 273.15;
 }
 
+// kilogram conversions
+export const KilogramsToGrams = (kilograms) => {
+    return kilograms * 1000;
+}
+
+export const KilogramsToMiligrams = (kilograms) => {
+    return kilograms * 1000000;
+}
+
+export const KilogramsToPounds = (kilograms) => {
+    return kilograms * 2.2046;
+}
+
+export const KilogramsToOunce = (kilograms) => {
+    return kilograms * 35.274;
+}
+
+// gram conversions
+export const GramsToKilograms = (grams) => {
+    return grams / 1000;
+}
+
+export const GramsToMiligrams = (grams) => {
+    return grams * 1000;
+}
+
+export const GramsToPounds = (grams) => {
+    return grams / 453.6;
+}
+
+export const GramsToOunce = (grams) => {
+    return grams / 28.35;
+}
+
+// miligram conversions
+export const MiligramsToKilograms = (miligrams) => {
+    return miligrams / 1000000;
+}
+
+export const MiligramsToGrams = (miligrams) => {
+    return miligrams / 1000;
+}
+
+export const MiligramsToPounds = (miligrams) => {
+    return miligrams / 453600;
+}
+
+export const MiligramsToOunce = (miligrams) => {
+    return miligrams / 28350;
+}
+
+// pounds conversions
+export const PoundsToKilograms = (pounds) => {
+    return;
+}
+
+export const PoundsToGrams = (pounds) => {
+    return;
+}
+
+export const PoundsToMiligrams = (pounds) => {
+    return;
+}
+
+export const PoundsToOunce = (pounds) => {
+    return;
+}
